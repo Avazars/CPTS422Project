84ddc7933e6b1000bd22e3ac7ca147368b2e7ab5
JSadler <jadin.sadler@wsu.edu>
2022-09-21 15:05:09 -0700

    adding some files

---
+++ Deliverable1Project/src/commentCountCheckPackage/CommentCountCheck.java
@@ -0,0 +1,48 @@
+package commentCountCheckPackage;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+public class CommentCountCheck extends AbstractCheck{
+
+	private int count = 0;
+	//private static final String CATCH_MSG = "This is the end of a statement. ";
+	
+	@Override
+	public boolean isCommentNodesRequired() 
+	{
+		return true;
+	}
+	
+	@Override
+	public int[] getAcceptableTokens() {
+		int[] tokens = {TokenTypes.VARIABLE_DEF};
+		return tokens;
+	}
+
+	@Override
+    public void visitToken(DetailAST aAST) {
+		count ++;
+    	reportStyleError(aAST, " found");
+    }
+ 
+    private void reportStyleError(DetailAST aAST, String variableName) {
+        log(aAST.getLineNo(), "Comment" + variableName);
+    }
+
+
+    
+	@Override
+	public int[] getDefaultTokens() {
+		int[] tokens = {TokenTypes.SINGLE_LINE_COMMENT, TokenTypes.BLOCK_COMMENT_BEGIN};
+		return tokens;
+	}
+
+	@Override
+	public int[] getRequiredTokens() {
+		// TODO Auto-generated method stub
+		return new int[0];
+	}
+
+}
---
+++ Deliverable1Project/src/halsteadCheckPackage/HalsteadLengthCheck.java
@@ -0,0 +1,25 @@
+package halsteadCheckPackage;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+
+public class HalsteadLengthCheck extends AbstractCheck{
+
+	@Override
+	public int[] getAcceptableTokens() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] getDefaultTokens() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] getRequiredTokens() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
